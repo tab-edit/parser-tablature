f314b1f5de05a9e40da37054a6374337dea81b15
Stanley Ihesiulo <stanihe1901@gmail.com>
2021-08-27 17:15:59 -0400

    fixed problem where the parser couldn't parse a measure component connector (slide, hammer-on, pull-off) which contained dashes inbetween. Example: 7---p2 could not be parsed as a pull-off from 7 to 2

--- src/tokens.js
+++ src/tokens.js
@@ -5,13 +5,21 @@ import {
   newlineEmpty,
   incomingNewline,
   incomingEOF,
+  incomingComponentConnector,
   stafflineSep,
+  connectorSymb,
+  graceSym,
 } from "./parser.terms.js";
 
 const newline = 10,
   carriageReturn = 13,
   space = 32,
-  tab = 9;
+  tab = 9,
+  dash = 45,
+  u = 117,
+  g = 103,
+  s = 115,
+  G = 71;
 
 export const newlines = new ExternalTokenizer((input) => {
   if (input.next < 0) {
@@ -34,21 +42,54 @@ export const newlines = new ExternalTokenizer((input) => {
   }
 });
 
-export const lookaheads = new ExternalTokenizer((input) => {
-  if (input.next < 0) {
-    input.acceptToken(incomingEOF);
-  } else if (input.next == newline || input.next == carriageReturn) {
-    input.acceptToken(incomingNewline);
-  }
-}, {fallback:true});
-
-export const stafflineSeparator = new ExternalTokenizer((input) => {
-  if (input.next == space || input.next == tab || input.next == newline || input.next == carriageReturn || input.next < 0) {
-    input.acceptToken(stafflineSep);
-  } else {
-    let prevChar = input.peek(-1);
-    if (prevChar == space || prevChar == tab) {
-      input.acceptToken(stafflineSep);
+export const lookaheads = new ExternalTokenizer(
+  (input) => {
+    if (input.next < 0) {
+      input.acceptToken(incomingEOF);
+    } else if (input.next == newline || input.next == carriageReturn) {
+      input.acceptToken(incomingNewline);
     }
   }
-}, {fallback:true});
+);
+
+export const stafflineSeparator = new ExternalTokenizer(
+  (input) => {
+    if (
+      input.next == space ||
+      input.next == tab ||
+      input.next == newline ||
+      input.next == carriageReturn ||
+      input.next < 0
+    ) {
+      input.acceptToken(stafflineSep);
+    } else {
+      let prevChar = input.peek(-1);
+      if (prevChar == space || prevChar == tab) {
+        input.acceptToken(stafflineSep);
+      }
+    }
+  },
+  { fallback: true }
+);
+
+export const measureComponentExternTokens = new ExternalTokenizer(
+  (input, stack) => {
+    if (input.next == g || input.next == G) {
+      input.advance();
+      if (isNum(input.next)) input.acceptToken(graceSym);
+    } else {
+      while (input.next == dash || input.next == space || input.next == tab) {
+        input.advance();
+      }
+      if (stack.canShift(connectorSymb)) {
+        //stack.canShift(connectorSymb)) {
+        input.acceptToken(incomingComponentConnector);
+      }
+    }
+  },
+  { contextual: true, fallback:true }
+);
+
+function isNum(charCode) {
+  return charCode >= 48 && charCode <= 57;
+}
